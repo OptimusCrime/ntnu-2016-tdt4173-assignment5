b9da740f03f8f2e5fddd83e2b351bbe469495afc
Håkon Ødegård Løvdal <me@hakloev.no>
2016-04-16 16:09:54 +0200

    Adds SVM test, a lot to do in terms of tweaking and configuration

--- models/models.py
+++ models/models.py
@@ -2,6 +2,7 @@ import logging
 
 from load.pickling import pickle_data, unpickle_data
 from sklearn import neighbors
+from sklearn import svm
 
 log = logging.getLogger(__name__)
 
@@ -13,4 +14,12 @@ def k_nearest_neighbours(from_pickle=False):
         # TODO: Return pickled model
 
     return neighbors.KNeighborsClassifier()
+
+def support_vector_machine(from_pickle=False):
+    log.info('Initiation SVM model')
+    if from_pickle:
+        pass
+        # TODO: Return pickled model
+
+    return svm.SVC(gamma=0.001)
 
